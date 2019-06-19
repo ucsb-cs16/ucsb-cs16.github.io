c0b6d3965a85bbe57dc86b585127f1c340f5522f
Phill Conrad <pconrad@cs.ucsb.edu>
2019-06-19 14:01:45 -0700

    update main cs16 page to point to summer

--- jekyll.sh
+++ jekyll.sh
@@ -2,5 +2,9 @@
 
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-rvm use 2.5.1
+rvm use 2.5.1 --install
+gem install bundler 
+gem install pkg-config
+gem install nokogiri
+bundle install --path vendor/bundle
 bundle exec jekyll serve $@
