ccf4df32badd95c6c7a0a78fff5f342ae0050091
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-12-20 13:19:16 -0800

    pc - try upgrading to Ruby 2.7.7

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 # If rebuilding Gemfile.lock, be sure to do this so that GitHub Actions works
 # bundle lock --add-platform x86_64-linux 
 
-ruby "2.7.4"
+ruby "2.7.7"
 source "https://rubygems.org"
 gem "jekyll"
 gem "just-the-docs"
--- jekyll.sh
+++ jekyll.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 
+rm Gemfile.lock
+bundle install
+bundle lock --add-platform x86_64-linux --add-platform x86_64-darwin-21
+
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 rvm use
 rm -rf _site
